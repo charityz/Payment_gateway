3fb3917344303513e177b022da72b5f11db6454e
charityz <ezecharity194@gmail.com>
2025-09-17 15:33:37 +0100

    fix transfer tab

--- Backend/main.py
+++ Backend/main.py
@@ -37,6 +37,7 @@ origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "https://payment-gateway-3.onrender.com",
+    "http://localhost:3000",
     'https://payverge.netlify.app'
 ]
 
